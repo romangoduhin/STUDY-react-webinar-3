c9b53ebbf4397db2dee67537c77a0c733d08c7e0
romangoduhin <superchewits2000@gmail.com>
2023-05-13 14:13:51 +0300

    TASK-3 fix incorrect displaying of selectedCount

--- src/app.js
+++ src/app.js
@@ -28,7 +28,7 @@ function App({store}) {
                   <div className='Item-code'>{item.code}</div>
                   <div className='Item-title'>
                     {item.title}
-                    {(!!item.selectedCount && item.selected) && ` | Выделяли ${item.selectedCount} раз`}
+                    {!!item.selectedCount && ` | Выделяли ${item.selectedCount} раз`}
                   </div>
                   <div className='Item-actions'>
                     <button onClick={() => store.deleteItem(item.code)}>
