c7f6a86f40ef23fd2aa0604a59c549e9ca388149
romangoduhin <superchewits2000@gmail.com>
2023-05-26 16:32:56 +0300

    TASK-2: implement product-page

--- src/components/item/index.js
+++ src/components/item/index.js
@@ -1,22 +1,21 @@
-import {memo, useState} from "react";
+import {memo} from "react";
 import PropTypes from "prop-types";
 import {cn as bem} from '@bem-react/classname';
 import {numberFormat} from "../../utils";
 import './style.css';
+import {Link} from "react-router-dom";
 
-function Item(props){
-
+function Item(props) {
   const cn = bem('Item');
 
   const callbacks = {
-    onAdd: (e) => props.onAdd(props.item._id)
+    onAdd: () => props.onAdd(props.item._id)
   }
 
   return (
     <div className={cn()}>
-      {/*<div className={cn('code')}>{props.item._id}</div>*/}
       <div className={cn('title')}>
-        {props.item.title}
+        <Link to={`/product/${props.item._id}`} className={cn('link')}>{props.item.title}</Link>
       </div>
       <div className={cn('actions')}>
         <div className={cn('price')}>{numberFormat(props.item.price)} ₽</div>
@@ -36,7 +35,8 @@ Item.propTypes = {
 };
 
 Item.defaultProps = {
-  onAdd: () => {},
+  onAdd: () => {
+  },
 }
 
 export default memo(Item);
